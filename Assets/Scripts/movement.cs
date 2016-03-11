94db379c2335b78f1de492ddf2f7e6bc3823b5d5
s1cyan <claudiab.yan@gmail.com>
2016-03-10 23:54:30 -0500

    rotate for character controller works. also renamed snowman in the grow script

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	public Transform snowman;
+	private float rotationRate = 3f;
+
+
+
+	void Awake(){
+		snowman = GetComponent<Transform> ();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotation = Input.GetAxis ("Horizontal") * rotationRate;
+		snowman.Rotate (0, rotation, 0);
+//		if (Input.GetKeyDown(KeyCode.RightArrow)){
+//			
+//			snowman.rotation = Quaternion ();
+//				}
+	}
+}
