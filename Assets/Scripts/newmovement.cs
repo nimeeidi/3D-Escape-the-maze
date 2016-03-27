774b79d130179c1c1a5953927588afbfb25d1bdc
Claudia Yan <claudiab.yan@gmail.com>
2016-03-26 23:55:09 -0400

    forward movement with rigidbody now works

--- Assets/Scripts/newmovement.cs
+++ Assets/Scripts/newmovement.cs
@@ -3,49 +3,54 @@ using System.Collections;
 
 public class newmovement : MonoBehaviour {
     private Rigidbody snowman;
-    public float rotateRate = 90f;
-    public float walkRate = 35f;
+    private float rotateRate = 90f;
+    private float walkRate = 3f;
 
     Quaternion holdsNextRotation; // this is the place you want to turn to
     private float turnInput;
     private float walkInput;
 
+    void Awake()
+    {
+        snowman = GetComponent<Rigidbody>();
+    }
 
 	void Start () {
         snowman = GetComponent<Rigidbody>();
         holdsNextRotation = transform.rotation; //gets current spawn coordinates
-        walkInput = turnInput = 0; //setting all the current inputs to 0
+        walkInput = 0;
+        turnInput = 0; //setting all the current inputs to 0
      
 	}
 
-    void GetInput()
+    private void GetInput()
     {
         walkInput = Input.GetAxis("Vertical"); //getAxis returns -1 to 1 
         turnInput = Input.GetAxis("Horizontal"); 
 
     }
 	
-	void Update () {
+	private void Update () {
         GetInput(); //gathers your input in update
         Turn(); //turning does not require physics
         
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Walk();
     }
 
-    void Walk()
+    private void Walk()
     {
-        snowman.velocity = transform.forward * walkInput * walkRate;
+        Vector3 move = transform.forward * walkInput * walkRate;
+        snowman.MovePosition(move+snowman.position);
         //transform.forward = moving on the snowman's forward axis
         //walkInput is pos/neg which determines forward/backward movement
         // walkRate multiplies the rate at which u want to go 
-        print("player is moving");
     }
 
-    void Turn()
+    private void Turn()
     {
         //Angle Axis takes angle from 0-360 that you want to turn and axis
         holdsNextRotation *= Quaternion.AngleAxis(rotateRate*turnInput*Time.deltaTime,Vector3.up); //vector3.up is global yaxis
